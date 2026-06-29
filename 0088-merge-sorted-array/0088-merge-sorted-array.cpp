d9583e43c7297df3c1beb76dfb8707b429de16dd
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-06-29 05:32:05 +0300

    Time: 0 ms (100%), Space: 12.3 MB (72.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,14 @@
+//update 2 -> optimized
 using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     int i = 0;
-    int size = nums1.size() - m;
-    while (i < size )
-    {
+
+    while (nums1.size() - m != 0 )
         nums1.pop_back();
-        i++;
-    }
-    i = 0;
-    size = nums2.size() - n;
-    while (i < size)
-    {
-        nums2.pop_back();
-        i++;
-    }
-    i = 0;
-     size = nums2.size();
-    while (i < size)
-    {
-        nums1.push_back(nums2[i]);
-        i++;
-    }
+    while (i < n)
+        nums1.push_back(nums2[i++]);
     sort(nums1.begin(), nums1.end());
     }
 };
