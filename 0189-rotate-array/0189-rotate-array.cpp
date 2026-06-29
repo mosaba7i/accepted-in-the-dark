533bac7401c0e4dc36fe39065b2949b824a767f4
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-06-29 06:46:39 +0300

    Time: 0 ms (100%), Space: 29.7 MB (44.22%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,28 +1,14 @@
+// update 2 -> optimized (learned and used reversed o(n) instead of putting erase in a while loop which causes o(n^2))
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         vector<int> tmp;
-        
-        
-        if (k > nums.size()) // will repeate for sure hehe
-        {
-            k = k % nums.size();
-        }
-        if (k == nums.size()) // i mean why not :)
+        k = k % nums.size();
+        if (k == 0) 
             return ;
-        int s = nums.size() - k;
-        int i = 0;
-        while (i < s)
-        {
-            tmp.push_back(nums[0]); // 1 2 3 4
-            nums.erase(nums.begin());
-            i++;
-        }
-        i = 0;
-        while (i < s)
-        {
-            nums.push_back(tmp[i]);
-            i++;
-        }
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin() ,nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());        
     }
 };
