e0cff2c6dd073586ee3be517d6212ad4abb08232
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-07-01 03:22:13 +0300

    Time: 4 ms (23.01%), Space: 52.4 MB (26.82%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,31 +1,17 @@
+// update 2 -> optimized (greedy solution )
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        bool status = 0;
-       vector<bool> visited(nums.size(), false);
-        function<void(vector<int>&,int)> bt = [&](vector<int>& nums, int i)
+        int distance = 0;
+        int i = 0;
+        while (i < nums.size())
         {
-            if (status || (i < nums.size() && visited[i]))
-                return;
-            int get_num = nums[i];
-            visited[i] = 1;
-            int j = 1;
-            if (i == nums.size()-1)
-            {
-                    status = 1;
-                    return;
-            }
-            while (i + j < nums.size() && j <= get_num)
-            {
-                if (!visited[i+j])
-                    bt(nums,i+j); 
-                if(status)
-                    return; 
-                j++;
-            }
-
-        } ;
-        bt(nums,0);
-        return(status);
+            if (i > distance)
+                return(false);
+            if (i + nums[i] > distance)
+                distance = i + nums[i];
+            i++;
+        }
+        return(true);
     }
 };
