6328c56bc1869e677a38b74219599158cf248779
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-07-01 09:16:28 +0300

    Time: 7 ms (52.14%), Space: 13.4 MB (37.25%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,74 +1,29 @@
-int get_char_value(char s,char s2,int *i)
-{
-    if (s2 == 'N')
-    {
-    if (s == 'I')
-        return(1);
-    if (s == 'V')
-        return(5);
-    if (s == 'X')
-        return(10);
-    if (s == 'L')
-        return(50);
-    if (s == 'C')
-        return(100);
-    if (s == 'D')
-        return(500);
-    if (s == 'M')
-        return(1000);
-    }
-    else if (s2)
-    {
-    if (s == 'I' && s2 =='V')
-        return(*i = *i + 1,4);
-    if (s == 'I' && s2 =='X')
-        return(*i = *i + 1,9);
-    if (s == 'X' && s2 =='L')
-        return(*i = *i + 1,40);
-    if (s == 'X' && s2 =='C')
-        return(*i = *i + 1,90);
-    if (s == 'C' && s2 =='D')
-        return(*i = *i + 1,400);
-    if (s == 'C' && s2 =='M')
-        return(*i = *i + 1,900);
-    else
-    {
-    if (s == 'I')
-        return(1);
-    if (s == 'V')
-        return(5);
-    if (s == 'X')
-        return(10);
-    if (s == 'L')
-        return(50);
-    if (s == 'C')
-        return(100);
-    if (s == 'D')
-        return(500);
-    if (s == 'M')
-        return(1000);
-    }
-    }
-    return(0);
-}
-int get_string_value(string s)
-{
-    string s1 = s;
-    int i = 0;
-    int sum = 0;
-    while (s1[i])
-    {
-        if (s1[i] && s1[i+1])
-            sum = get_char_value(s1[i],s1[i + 1],&i) + sum;
-        else if (s1[i] && !s[i+1])
-            sum = get_char_value(s1[i],'N',&i) + sum;
-        i++;
-    }
-    return (sum);
-}
+// optimzed (less code)
+// omg my last sol is so long thank god for discovering hash tables :)
 class Solution {
 public:
     int romanToInt(string s) {
-        return(get_string_value(s));
+        unordered_map<char,int> key=
+        {
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+        int sum = 0;
+        for(int i = 0;i < s.size();i++)
+        {
+            if (i+1 < s.size() && key[s[i]] < key[s[i+1]])
+            {
+                sum += key[s[i+1]] - key[s[i]];
+                i++;
+            }
+            else
+                sum += key[s[i]];
+        }
+        return(sum);
     }
 };
