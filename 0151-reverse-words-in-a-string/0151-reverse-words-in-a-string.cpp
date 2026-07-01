0c577c2024f972e7aa03070752003739d2028e86
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-07-01 09:53:49 +0300

    Time: 1 ms (46.87%), Space: 10.1 MB (79.93%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,34 +1,23 @@
-
+//less noise more optimized :p
 class Solution {
 public:
     string reverseWords(string s) {
-        int i = 0;
-        string str;
-        while (s[i])
-            i++;
-        i--;
-        int j = 0;
-        while (i >= 0)
-        {
-
-            while (i >= 0 && (s[i] == ' ' || s[i] == 9 ) )
-                i--;
-            if (i < 0)
-             break ;
-            while (i >= 0 && s[i] && (s[i] != ' ' && s[i] != 9 ))
-                i--; // points now to a space
-                j = i + 1;
-            if (!str.empty())
-                str.push_back(' ');
-            while (j >= 0 && s[j] && (s[j] != ' ' && s[j] != 9)  )
-            {
-                str.push_back(s[j]);
-                j++;
-            }
-            
-                
-            
-        }
+       int i = s.size() - 1;
+       int j = 0;
+       string str;
+       while(i >= 0)
+       {
+        while (i >= 0 && s[i] == ' ' )
+            i--;
+        if (i == -1)
+            break;
+        j = i;
+        while (i >= 0 && s[i] != ' ' )
+            i--;
+        if(!str.empty())
+            str+=' ';
+        str += s.substr(i+1,j-i);
+       }
         return(str);
     }
 };
