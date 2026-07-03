123de96fb790823396b7872a69c5b6ba65a9d7ee
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-07-04 02:00:57 +0300

    Time: 0 ms (100%), Space: 7.9 MB (49.35%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        while(n % 2 == 0 && n != 0)
+        {
+            n = n / 2;
+        }
+        if (n == 1)
+            return(true);
+        return(false);
+    }
+};
