3c647de370ac68d227c6b961838d9fa8bc6cb8a7
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-07-09 13:40:02 +0300

    Time: 39 ms (22.17%), Space: 25.3 MB (44.56%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -4,12 +4,11 @@ public:
         unordered_set <int> key;
         int count = 0;
         for(int i = 0;i < s.size();i++)
-        {
-            if(key.count(s[i]))
-                count--;
-            key.insert(s[i]);
-            count++;
-        }
+            if(!key.count(s[i]))
+            {
+                key.insert(s[i]);
+                count++;
+            }
         return(count);
     }
 };
