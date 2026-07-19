24a3f1abcd303bdb8442a0f83b26b2d7abfdaccb
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-07-19 13:49:00 +0300

    Time: 0 ms (100%), Space: 45.4 MB (37.35%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -5,12 +5,8 @@ public:
         int n = 0;
         for(int i = 0; i < nums.size();i++)
             sum += nums[i];
-        while(sum % k != 0 && sum >= 0)
-        {
-            sum--;
-            n++;
-        }
-        return(n);
+        
+        return(sum % k);
 
 
 
