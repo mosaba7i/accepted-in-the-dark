2d9e81b879012c920bf207f78c5d75417ad11711
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-07-21 17:20:49 +0300

    Time: 3 ms (10.23%), Space: 16.8 MB (60.34%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,9 +1,13 @@
+//another sol
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        int s = nums.size();
-        for(int i =0;i<s;i++)
-            nums.push_back(nums[i]);
-        return(nums);
+        vector<int> s(nums.size()*2);
+        for(int i =0;i<nums.size();i++)
+    {
+            s[i] = nums[i];
+            s[i + nums.size()] =nums[i];
+    }
+        return(s);
     }
 };
