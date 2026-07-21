a2995aae7617effa473d8744753f864658f6044f
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-07-21 16:44:15 +0300

    Time: 0 ms (100%), Space: 8.2 MB (74.19%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
         return {
-        (double)(celsius + 273.15),
-        (double)(celsius * 1.80 + 32.00)
+        celsius + 273.15,
+        celsius * 1.80 + 32.00
     };
     }
 };
