4581ece56d61e04b0147da3b0c37ef6fea5842f9
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-07-21 14:49:34 +0300

    Time: 0 ms (100%), Space: 20.9 MB (20.21%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector <int> n;
+        for(int i = 0; i < nums.size();i++)
+        n.push_back(nums[nums[i]]);
+        return (n);
+    }
+};
